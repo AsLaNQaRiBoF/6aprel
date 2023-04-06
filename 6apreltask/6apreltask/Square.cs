9c6f1cc90c60229939079b8151a04aeec4ca5a8b
aslan <aslanqaribov7@gmail.com>
2023-04-06 13:49:27 +0400

    first commit

---
+++ 6apreltask/6apreltask/Square.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6apreltask
+{
+    internal abstract class Square : Figure
+    {
+       
+          
+        private int _side;
+        public int Side
+        {
+            get { return _side; }
+            set
+            {if(value>0)
+                _side = value;
+            }
+
+        }
+
+
+        public override void CalcArea(int teref)
+        {
+            int square = teref*teref;
+        }
+    }
+}
